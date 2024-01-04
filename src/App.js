37815e0fc6096eb8e169bcadc4e4437e56a53bc7
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-01-04 16:23:58 +0100

    css in resct ternary operators and lists in react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 
 import './App.css';
+import {Users } from "./User"
 
+// function App() {
+//   const age = 17
+//   const isGreen = true
+//   return (
+//     <div className="App">
+//       {/* <h1 className='name'>Opebiyi</h1> */}
+//       {age < 18 ? <h1>Under age</h1> : <h1>Over age</h1>}
+//       <h1 style={{color: isGreen ? "red" : "green"}}>THis has color</h1>
+
+//       {isGreen && <button>This is a button</button>}
+//     </div>
+//   );
+// }
+
+// function App() {
+//   const names = ['femi', 'francis', 'wilson', 'neymar', 'higuain']
+//   return (
+//     <div className="App">
+//       {names.map((name, key)=>{
+//         return <h1 key={key}>{`${key + 1}: ${name}`}</h1>
+//       })}
+//     </div>
+//   );
+// }
 function App() {
+let peopleArray = [
+  { name: "John", age: 25 },
+  { name: "Emily", age: 30 },
+  { name: "Michael", age: 22 }
+];
+
   return (
     <div className="App">
-      <User name ="bola" age= {25} email = "bola.com"/>
-      <Job salary = {50000} position="Senior Dev" company="Spotify"/>
-      <User/>
+      {peopleArray.map((user, key)=>{
+      return <Users name={user.name} age={user.age}/>
+      })}
     </div>
   );
 }
+
+
 
 export default App;
 
---
+++ src/User.js
@@ -0,0 +1,4 @@
+export const Users = (props)=>{
+  return <div>{props.name} {props.age} </div>
+  
+}
