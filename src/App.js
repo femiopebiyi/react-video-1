1d234b02db623475ae96eb049b27b79e79f6833f
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-01-04 15:01:38 +0100

    simple classwork

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <User name ="bola" age= {25} email = "bola.com"/>
+      <Job salary = {50000} position="Senior Dev" company="Spotify"/>
       <User/>
     </div>
   );
@@ -26,5 +27,14 @@ const User = (props)=>{
     <h1>{props.name}</h1>
     <h1>{props.age}</h1>
     <h1>{props.email}</h1>
+  </div>
+}
+
+
+const Job = (props)=>{
+  return <div>
+    <h1>{props.salary}</h1>
+    <h1>{props.position}</h1>
+    <h1>{props.company}</h1>
   </div>
 }
