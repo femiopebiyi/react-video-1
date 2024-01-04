a9a94ac9344b13488465362b33dad51479b501e4
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-01-04 14:54:00 +0100

    components jsx and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name ="bola" age= {25} email = "bola.com"/>
+      <User/>
     </div>
   );
 }
 
 export default App;
+
+
+// const getName = () =>{
+//   return "Pedro"
+// }
+
+// const GetNameComponent = ()=>{
+//   return <h2>Pedro</h2>
+// }
+
+const User = (props)=>{
+  return <div>
+    <h1>{props.name}</h1>
+    <h1>{props.age}</h1>
+    <h1>{props.email}</h1>
+  </div>
+}
