96478722df69889d3d69a23cd7b29b6954509966
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2024-01-17 14:07:11 +0100

    chatgpt saved me (prop drilling)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 
 import './App.css';
-import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Axios from "axios";
+import { useState} from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
 import { Contact } from './pages/Contact';
@@ -11,16 +10,18 @@ import { Navbar } from './pages/Navbar';
 
 
 function App() {
-  const [username, setUsername] = useState("Femi")
+  const [username, setUsername] = useState("Femi");
+    
   return (
     <div className="App">
       <Router>
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home username={username}/>}/>
-          <Route path='/menu' element={<Profile username={username}/>} setUsername={setUsername}/>
+          {/* Fix: Pass setUsername prop inside the element prop */}
+          <Route path='/profile' element={<Profile username={username} setUsername={setUsername} />} />
           <Route path='/contact' element={<Contact/>}/>
-          <Route path='/' element={<h1>Page not found</h1>}/>
+          <Route path='*' element={<h1>Page not found</h1>}/> {/* Adjusted route for 404 page */}
         </Routes>
       </Router>
     </div>
@@ -31,20 +32,3 @@ function App() {
 export default App;
 
 
-// const TopComponent = ()=>{
-//   const [state, setState] = useState()
-
-//   return <div>
-//         <MiddleComponent state = {state}/>
-//   </div>
-// }
-// const MiddleComponent = ()=>{
-
-//   return <div>
-//         <BottomComponent state = {state}/>
-//   </div>
-// }
-
-// function BottomComponent (state){
-//   return <div>{state}</div>
-// }
